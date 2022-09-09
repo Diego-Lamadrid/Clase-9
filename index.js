a9ca53432b2135bb278b31bcffadd5aa23bab3cf
Diego-Lamadrid <112910313+Diego-Lamadrid@users.noreply.github.com>
2022-09-09 20:08:53 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+var pais = "españa";
+var continente = "Europa";
+var antiguedad = "2021";
+var pais_y_continente = pais + "fdsfg" + continente;
+
+pais = " Mexico";
+continente = "Latinoamerica";
+
+console.log(pais, continente, antiguedad);
+alert(pais_y_contiente);
